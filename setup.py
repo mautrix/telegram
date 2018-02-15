25edade2a9bf5b66c075fcbbfcca0458e1251e28
Tulir Asokan <tulir@maunium.net>
2018-02-15 20:38:02 +0200

    Use tuple comparison syntax when checking which Telethon branch to install

--- setup.py
+++ setup.py
@@ -26,10 +26,9 @@ setuptools.setup(
         "python-magic>=0.4.15,<0.5",
     ],
     dependency_links=[
-        (
-            "https://github.com/LonamiWebs/Telethon/tarball/7da092894b306d720cc60c04daa2bfba58f81946#egg=Telethon"
-            if sys.version_info.major > 3 or sys.version_info.minor > 5
-            else "https://github.com/tulir/Telethon/tarball/ac46abc9680c5a74897fe6dbe9e585ad2577b1fa#egg=Telethon")
+        ("https://github.com/LonamiWebs/Telethon/tarball/7da092894b306d720cc60c04daa2bfba58f81946#egg=Telethon"
+         if sys.version_info > (3, 5)
+         else "https://github.com/tulir/Telethon/tarball/ac46abc9680c5a74897fe6dbe9e585ad2577b1fa#egg=Telethon")
     ],
 
     classifiers=[
