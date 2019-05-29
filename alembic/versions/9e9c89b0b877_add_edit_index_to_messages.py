ff257fcd7759b6d1f4dd49b668dace409e0a5fb5
Tulir Asokan <tulir@maunium.net>
2019-05-29 16:37:13 +0300

    Fix edit index upgrade on postgres

--- alembic/versions/9e9c89b0b877_add_edit_index_to_messages.py
+++ alembic/versions/9e9c89b0b877_add_edit_index_to_messages.py
@@ -8,7 +8,6 @@ Create Date: 2019-05-29 15:28:23.128377
 from alembic import op
 import sqlalchemy as sa
 
-
 # revision identifiers, used by Alembic.
 revision = '9e9c89b0b877'
 down_revision = '17574c57f3f8'
@@ -24,14 +23,13 @@ def upgrade():
                     sa.Column('tg_space', sa.Integer),
                     sa.Column('edit_index', sa.Integer),
                     sa.PrimaryKeyConstraint('tgid', 'tg_space', 'edit_index'),
-                    sa.UniqueConstraint("mxid", "mx_room", "tg_space", name="_mx_id_room"))
+                    sa.UniqueConstraint("mxid", "mx_room", "tg_space", name="_mx_id_room_2"))
     c = op.get_bind()
     c.execute("INSERT INTO _message_temp (mxid, mx_room, tgid, tg_space, edit_index) "
               "SELECT message.mxid, message.mx_room, message.tgid, message.tg_space, 0 "
               "FROM message")
     c.execute("DROP TABLE message")
     c.execute("ALTER TABLE _message_temp RENAME TO message")
-
 
 
 def downgrade():
@@ -45,6 +43,6 @@ def downgrade():
     c = op.get_bind()
     c.execute("INSERT INTO _message_temp (mxid, mx_room, tgid, tg_space) "
               "SELECT message.mxid, message.mx_room, message.tgid, message.tg_space "
-              "FROM portal")
+              "FROM message")
     c.execute("DROP TABLE message")
     c.execute("ALTER TABLE _message_temp RENAME TO message")
