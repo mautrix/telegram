9899c15d36ca22b4f6f4c7ec319c951e474e56d1
Tulir Asokan <tulir@maunium.net>
2019-08-07 19:49:22 +0300

    Handle potential error kicking user

--- mautrix_telegram/portal/metadata.py
+++ mautrix_telegram/portal/metadata.py
@@ -508,7 +508,10 @@ class PortalMetadata(BasePortal, ABC):
                     return
                 except MForbidden:
                     pass
-            await self.main_intent.kick_user(self.mxid, user.mxid, kick_message)
+            try:
+                await self.main_intent.kick_user(self.mxid, user.mxid, kick_message)
+            except MForbidden as e:
+                self.log.warn(f"Failed to kick {user.mxid}: {e}")
 
     async def update_info(self, user: 'AbstractUser', entity: TypeChat = None) -> None:
         if self.peer_type == "user":
