a565853c5e57a5671ebe4da178a5ac3d02a5005c
Tulir Asokan <tulir@maunium.net>
2020-09-18 17:37:31 +0300

    Update things in setup.py

--- setup.py
+++ setup.py
@@ -61,20 +61,15 @@ setuptools.setup(
         "Framework :: AsyncIO",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
     ],
-    entry_points="""
-        [console_scripts]
-        mautrix-telegram=mautrix_telegram.__main__:main
-    """,
     package_data={"mautrix_telegram": [
         "web/public/*.mako", "web/public/*.png", "web/public/*.css",
         "example-config.yaml",
     ]},
     data_files=[
-        (".", ["alembic.ini"]),
+        (".", ["alembic.ini", "mautrix_telegram/example-config.yaml"]),
         ("alembic", ["alembic/env.py"]),
         ("alembic/versions", glob.glob("alembic/versions/*.py"))
     ],
