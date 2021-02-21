6020cdf8bf18b35d4a8633d0f813dc2239af3219
Tulir Asokan <tulir@maunium.net>
2021-02-21 17:24:35 +0200

    Let mautrix-python handle registration generation message

--- docker-run.sh
+++ docker-run.sh
@@ -26,9 +26,6 @@ fi
 
 if [ ! -f /data/registration.yaml ]; then
 	python3 -m mautrix_telegram -g -c /data/config.yaml -r /data/registration.yaml
-	echo "Didn't find a registration file."
-	echo "Generated one for you."
-	echo "Copy that over to synapses app service directory."
 	fixperms
 	exit
 fi
