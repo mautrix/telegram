5a76b5bcdc534d8f824bafa76ab86d7bd3081a2e
Sumner Evans <me@sumnerevans.com>
2021-08-04 14:03:54 -0600

    Upgrade mautrix to 0.10.2 and use new BridgeStateEvents

--- mautrix_telegram/__main__.py
+++ mautrix_telegram/__main__.py
@@ -129,5 +129,8 @@ class TelegramBridge(Bridge):
     def is_bridge_ghost(self, user_id: UserID) -> bool:
         return bool(Puppet.get_id_from_mxid(user_id))
 
+    async def count_logged_in_users(self) -> int:
+        return len([user for user in User.by_tgid.values() if user.tgid])
+
 
 TelegramBridge().run()
