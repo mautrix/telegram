8603c67347e6865dff3db0a169d1cd0387c91991
Tulir Asokan <tulir@maunium.net>
2021-12-12 12:17:54 +0200

    Move format_duration to mautrix-python
    Closes #707

--- mautrix_telegram/util/__init__.py
+++ mautrix_telegram/util/__init__.py
@@ -1,5 +1,4 @@
 from .file_transfer import transfer_file_to_matrix, convert_image
 from .parallel_file_transfer import parallel_transfer_to_telegram
-from .format_duration import format_duration
 from .recursive_dict import recursive_del, recursive_set, recursive_get
 from .color_log import ColorFormatter
