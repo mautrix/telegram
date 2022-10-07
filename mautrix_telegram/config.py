9239effb07e8744e2d751f26a829880b0252fc98
Andrew Ferrazzutti <andrewf@element.io>
2022-10-07 00:55:48 -0400

    Add `__future__` import for py3.8 compatibility

--- mautrix_telegram/config.py
+++ mautrix_telegram/config.py
@@ -13,6 +13,8 @@
 #
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
+from __future__ import annotations
+
 from typing import Any, List, NamedTuple
 import os
 
