1b2f07dfa292afcb15817edc77bb32c3b71baf38
Nick Mills-Barrett <nick@fizzadar.com>
2023-11-13 18:21:39 +0000

    Add quick retries when connecting to Telegram (#941)
    * Add quick 5 retries when connecting to Telegram
    
    * Fix attempt initialisation
    
    Co-authored-by: Tulir Asokan <tulir@maunium.net>
    
    * Log only when retrying
    
    Co-authored-by: Tulir Asokan <tulir@maunium.net>
    
    ---------
    
    Co-authored-by: Tulir Asokan <tulir@maunium.net>

--- mautrix_telegram/abstract_user.py
+++ mautrix_telegram/abstract_user.py
@@ -306,7 +306,16 @@ class AbstractUser(ABC):
     async def start(self, delete_unless_authenticated: bool = False) -> AbstractUser:
         if not self.client:
             await self._init_client()
-        await self.client.connect()
+        attempts = 1
+        while True:
+            try:
+                await self.client.connect()
+            except Exception:
+                attempts += 1
+                if attempts > 10:
+                    raise
+                self.log.exception("Exception connecting to Telegram, retrying in 5s...")
+                await asyncio.sleep(5)
         self.log.debug(f"{'Bot' if self.is_relaybot else self.mxid} connected: {self.connected}")
         return self
 
