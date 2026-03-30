c358222ab4eb070b7a187794cc3686f085bc3bb6
Tulir Asokan <tulir@maunium.net>
2026-03-30 20:57:48 +0300

    store: fix telegram_file migration delete query on sqlite

--- pkg/connector/store/upgrades/07-telegram-file-fixes.sql
+++ pkg/connector/store/upgrades/07-telegram-file-fixes.sql
@@ -1,4 +1,4 @@
 -- v7 (compatible with v2+): Add index and fix values in telegram_file
-DELETE FROM telegram_file tf1 WHERE id LIKE '%-' AND EXISTS(SELECT 1 FROM telegram_file tf2 WHERE tf2.id=REPLACE(tf1.id, '-', ''));
+DELETE FROM telegram_file WHERE id LIKE '%-' AND EXISTS(SELECT 1 FROM telegram_file tf2 WHERE tf2.id=REPLACE(telegram_file.id, '-', ''));
 UPDATE telegram_file SET id=REPLACE(id, '-', '') WHERE id LIKE '%-';
 CREATE INDEX telegram_file_mxc_idx ON telegram_file (mxc);
