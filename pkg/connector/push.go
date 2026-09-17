36ff227d0c32dc07ebae5f3b8fea105bb5632bc7
Gerardo Rodriguez <g@rodrz.com>
2026-09-17 10:09:02 -0400

    push: support encrypted push relay notifications (#1099)

--- pkg/connector/push.go
+++ pkg/connector/push.go
@@ -45,6 +45,7 @@ var (
 )
 
 var PushAppSandbox = false
+var PushConfig = &bridgev2.PushConfig{Native: true}
 
 type PushCustomData struct {
 	MessageID int `json:"msg_id,string"`
@@ -258,6 +259,15 @@ var FullSyncOnConnectBackground = false
 
 func (tc *TelegramClient) ConnectBackground(ctx context.Context, params *bridgev2.ConnectBackgroundParams) error {
 	data, _ := params.ExtraData.(*PushNotificationData)
+	if data == nil && len(params.RawData) > 0 {
+		loginID, parsed, err := tc.main.ParsePushNotification(ctx, params.RawData)
+		if err != nil {
+			return err
+		} else if loginID != tc.userLogin.ID {
+			return fmt.Errorf("push notification belongs to a different login")
+		}
+		data = parsed.(*PushNotificationData)
+	}
 	var relatedPortal *bridgev2.Portal
 	var sender *bridgev2.Ghost
 	var messageID networkid.MessageID
@@ -330,6 +340,9 @@ func (tc *TelegramClient) ConnectBackground(ctx context.Context, params *bridgev
 
 func (tc *TelegramConnector) ParsePushNotification(ctx context.Context, data json.RawMessage) (networkid.UserLoginID, any, error) {
 	val := gjson.GetBytes(data, "p")
+	if !val.Exists() {
+		val = gjson.GetBytes(data, "data.p")
+	}
 	if val.Type != gjson.String {
 		return "", nil, fmt.Errorf("missing or invalid p field")
 	}
@@ -430,5 +443,5 @@ func (tc *TelegramClient) RegisterPushNotifications(ctx context.Context, pushTyp
 }
 
 func (tc *TelegramClient) GetPushConfigs() *bridgev2.PushConfig {
-	return &bridgev2.PushConfig{Native: true}
+	return PushConfig
 }
